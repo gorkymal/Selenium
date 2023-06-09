a56ca398fae0fc0fd436b214738b469799aa855a
Grigoriy <sir.mal-grisha@yandex.ru>
2023-06-09 13:22:11 +0300

    selenium project

---
+++ src/test/java/HealthPage.java
@@ -0,0 +1,44 @@
+import com.beust.jcommander.Parameter;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HealthPage {
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
